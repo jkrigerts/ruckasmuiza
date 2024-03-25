e4e3a9341839523675e4b32fdd6dfab226ead574
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-03-25 11:06:09 +0200

    RUC-17 Checkpoint
    Made container, and container text
    this checkpoint is right before making calendar.

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,3 +1,18 @@
 <x-layouts.app>
-  <h1 class="font-playfair text-3xl text-center">Afiša</h1>
+
+    <section class="main-events">
+        <img class="screen-image" src="{{ asset('images/landing/landing7.webp') }}" alt="">
+        
+        <div class="content">
+            <div class="flex-between">
+                <p class="header">Ruckas muiža ielūdz!</p>
+                <p class="date">2024. gada <strong>MARTA PASĀKUMU KALENDĀRS</strong></p>
+            </div>
+            <p>Uzziniet vairāk, izvēloties krāsaino datumu</p>
+
+            <table>
+                
+            </table>
+        </div>
+    </section>
 </x-layouts.app>
