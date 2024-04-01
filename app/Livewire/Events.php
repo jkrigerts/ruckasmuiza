55b56d70f942dd89b8d739fb0f6447a63b558a6c
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-04-01 16:45:01 +0300

    Checkpoint before url function
    Just made front end arrows to work, and im going to develop url param function

--- app/Livewire/Events.php
+++ app/Livewire/Events.php
@@ -1,43 +1,28 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Livewire;
 
-use App\Models\Events;
+use Livewire\Component;
+
+use App\Models\Events as EventsModal;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
-class EventsController extends Controller
+class Events extends Component
 {
-    /**
-     * This is a PHP code snippet for the EventsController class.
-     * 
-     * The index method is responsible for handling the request and returning the view for the 'events' page.
-     * It takes a Request object as a parameter and validates the 'month' and 'year' parameters.
-     * If the validation passes, it sets the current date based on the provided month and year, or uses the current date if not provided.
-     * It then calculates the first and last day of the month and stores them in variables.
-     * 
-     * The method creates an empty array called $return to store the dates and events.
-     * It adds the days before the current month to the $return array.
-     * It then adds the days of the current month to the $return array, along with any events that happen on those dates.
-     * Finally, it adds the days after the current month to the $return array if needed.
-     * 
-     * The $return array is then dumped using the dd() function for debugging purposes.
-     * The method returns the 'events' view.
-     */
-    public function index(Request $req){
+    public $calendarData;
+    public $previousMonth;
+    public $thisMonth;
+    public $nextMonth;
+    public $monthsSelect;
+    public $thisYear;
+
+    public $monthOffset = 0; // this is the way arrow buttons are going to work now()->addMonths($monthOffset)
+
+    public function data(){
         $dateFormat = 'Y-m-d';
-        $req->validate([
-            'month' => 'numeric|integer|between:1,12',
-            'year' => 'numeric|integer|between:'. now()->year - 2 .','. now()->year + 2 .''
 
-        ]);
-
-        $today = isset($req->month) ? Carbon::parse(now()->year . '-' . $req->month . '-' . now()->day) : now();
-        $today = Carbon::parse(
-            (isset($req->year) ? $req->year : now()->year) . '-' .
-            (isset($req->month) ? $req->month : now()->month) . '-' .
-            now()->day
-        );
+        $today = Carbon::parse(now()->addMonths($this->monthOffset)->format($dateFormat));
 
         $monthsKa = [ // atbild uz jautājumu Kā?
             'janvāra',
@@ -81,7 +66,7 @@ class EventsController extends Controller
         for($i = $firstDayPlace - 1; $i >= 1; $i--){
             $day = Carbon::parse($today)->startOfMonth()->subDays($i);
             $dayName = $day->format($dateFormat);
-            $events = Events::where('happens_at', $dayName)->with('type')->get();
+            $events = EventsModal::where('happens_at', $dayName)->with('type')->get();
 
             $return[$dayName] = [
                 'monthName' => $monthsKas[$day->month - 1],
@@ -95,7 +80,7 @@ class EventsController extends Controller
         // add this month days
         $currentDate = $firstDayOfMonth;
         while ($currentDate <= $lastDayOfMonth) {
-            $events = Events::where('happens_at', $currentDate)->with('type')->get();
+            $events = EventsModal::where('happens_at', $currentDate)->with('type')->get();
             
             $return[$currentDate] = [
                 'monthName' => $monthsKas[Carbon::parse($currentDate)->month - 1],
@@ -111,7 +96,7 @@ class EventsController extends Controller
         $currentDate = $lastDayOfMonth;
         while(count($return) < 35){
             $currentDate = Carbon::parse($currentDate)->addDay()->format($dateFormat);
-            $events = Events::where('happens_at', $currentDate)->with('type')->get();
+            $events = EventsModal::where('happens_at', $currentDate)->with('type')->get();
             
             $return[$currentDate] = [
                 'monthName' => $monthsKas[Carbon::parse($currentDate)->month - 1],
@@ -122,14 +107,31 @@ class EventsController extends Controller
             ];
         }
         
-        return view('events', [
-            'calendarData' => array_chunk($return, 7, true),
-            'previousMonth' => $monthsKas[$today->subMonth()->month - 1],
-            'thisMonth' => $monthsKa[$today->addMonth()->month - 1],
-            'nextMonth' => $monthsKas[$today->addMonth()->month - 1],
-            'monthsSelect' => $monthsKa,
-            'thisYear' => $today->year,
+        
+        $this->calendarData = array_chunk($return, 7, true);
+        $this->previousMonth = $monthsKas[$today->subMonth()->month - 1];
+        $this->thisMonth = ucfirst($monthsKas[$today->addMonth()->month - 1]);
+        $this->nextMonth = $monthsKas[$today->addMonth()->month - 1];
+        $this->monthsSelect = $monthsKa;
+        $this->thisYear = $today->year;
 
-        ]);
+    }
+
+    public function render(Request $req)
+    {
+        $this->data();
+        return view('livewire.events');
+    }
+
+
+    // TODO maybe: add checks and limitations for users
+    public function up(){
+        $this->monthOffset++;
+        $this->data();
+    }
+
+    public function down(){
+        $this->monthOffset--;
+        $this->data();
     }
 }
