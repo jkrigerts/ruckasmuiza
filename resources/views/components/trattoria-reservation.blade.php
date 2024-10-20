b15d0f33f3cd73711328c9ef2afe0a785fa27697
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-10-20 10:27:49 +0300

    Order

--- resources/views/components/trattoria-reservation.blade.php
+++ resources/views/components/trattoria-reservation.blade.php
@@ -1,5 +1,6 @@
 <x-two-columns img_src='images/trattoria/trattoria_25.webp' img_alt="Ruckas muižas Trattoria ceptā pica">
   <x-slot:title>Rezervācija</x-slot:title>
-  <p>Ir iespējams veikt galdiņu rezervāciju, kā arī iepriekš pasūtīt picu un ierasties tai pakaļ sev ērtā laikā, zvanot uz <a href="tel:+37129407566">+371&nbsp;29407566</a>.</p>
+  <p>Ir iespējams veikt galdiņu rezervāciju, kā arī iepriekš pasūtīt picu un ierasties tai pakaļ sev ērtā laikā, zvanot uz <a href="tel:+37129407566">+371&nbsp;29407566</a>
+    (papildus tiek piemērota maksa par iepakojumu: 1 € katrai picai).</p>
   <p>Tāpat arī piedāvājam <a href="{{route('trattoria.piegade')}}">picu piegādi uz mājām</a> Cēsīs un apkārtnē!</p>
 </x-two-columns>
