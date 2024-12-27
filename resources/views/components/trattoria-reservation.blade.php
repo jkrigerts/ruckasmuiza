9e69b199703a79fe288dd0738a0eb45535d62745
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-12-27 21:53:14 +0200

    Wolt piegāde

--- resources/views/components/trattoria-reservation.blade.php
+++ resources/views/components/trattoria-reservation.blade.php
@@ -2,5 +2,5 @@
   <x-slot:title>Rezervācija</x-slot:title>
   <p>Ir iespējams veikt galdiņu rezervāciju, kā arī iepriekš pasūtīt picu un ierasties tai pakaļ sev ērtā laikā, zvanot uz <a href="tel:+37129407566">+371&nbsp;29407566</a>
     (papildus tiek piemērota maksa par iepakojumu: 1 € katrai picai).</p>
-  <p>Tāpat arī piedāvājam <a href="{{route('trattoria.piegade')}}">picu piegādi uz mājām</a> Cēsīs un apkārtnē!</p>
+  <p>Tāpat arī piedāvājam picu piegādi uz mājām sadarbībā ar <a href="https://wolt.com/en/lva/cesis/restaurant/trattoria-cesis" target="_blank"><em>Wolt</em></a>.</p>
 </x-two-columns>
