bcf2cca86029f135bf7f9e561c15a8fb88918cda
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2025-04-19 11:03:12 +0300

    Ēdienkarte

--- resources/views/trattoria/edienkarte.blade.php
+++ resources/views/trattoria/edienkarte.blade.php
@@ -6,7 +6,7 @@
   <div class="trattoria-centered-btn" style="margin-bottom:5rem">
     <a href="{{ url('assets/Ruckas_muiza-Trattoria-edienkarte.pdf') }}" class="button-link" target="_blank">Apskatīt ēdienkarti</a>
 
-    <a href="{{ url('assets/Rucka_manor-Trattoria-menu.pdf') }}" class="button-link" target="_blank">View Menu in English</a>
+    {{-- <a href="{{ url('assets/Rucka_manor-Trattoria-menu.pdf') }}" class="button-link" target="_blank">View Menu in English</a> --}}
   </div>
 
   <x-trattoria-reservation></x-trattoria-reservation>
